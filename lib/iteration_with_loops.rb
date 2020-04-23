1608f08fe06012ea58c01de58388cc2bc2310e4d
Jonathan T Dean <jonathantomdean@gmail.com>
2020-04-23 20:05:14 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,8 +3,23 @@ def join_nested_strings(src)
 
   src.each do | i |
     # puts i
-    full_string << i.join(" ")
-  end
+    # i.each do | x |
+      # puts x
+      i.each do |is_string|
+        if is_string.class == String
+          full_string << is_string
+          
+        end
+        # puts is_string.class
+      end
+    # if i.class == String
+    # 
 
-  return full_string
+      # puts full_string
+    # end
+    # combine.join(" ")
+  end
+  
+  return full_string.join(" ")
+
 end
